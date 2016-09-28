e55ec5fdb725ebe2efbfbe2d685e8e8fb2f72621
likang <likangios@163.com>
2016-09-28 17:53:00 +0800

    init

---
+++ FirstUseCarthageDemo/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  FirstUseCarthageDemo
+//
+//  Created by FengLing on 16/9/28.
+//  Copyright © 2016年 lk. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+import SwiftyJSON
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.grayColor()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
